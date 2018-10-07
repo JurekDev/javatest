81e41b49c14ac15bdb476358bb705270ab510d18
Wilko Müller <wimueller@uos.de>
2018-10-07 12:00:49 +0200

    deleted a line

--- index.js
+++ index.js
@@ -5,7 +5,6 @@
  * This sample demonstrates a simple skill built with the Amazon Alexa Skills
  * nodejs skill development kit.
  * This sample supports multiple lauguages. (en-US, en-GB, de-DE).
- * The Intent Schema, Custom Slots and Sample Utterances for this skill, as well
  * as testing instructions are located at https://github.com/alexa/skill-sample-nodejs-fact
  **/
 
